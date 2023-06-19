f065f901a78a511410defa8ac53a782e134e6a3f
falco1717 <74680727+falco1717@users.noreply.github.com>
2023-06-19 10:35:51 -0700

    Add files via upload

---
+++ Tactical-RMM-Saltbox/cert-dumper/cert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# uodates tactical-rmm.env file with cert info
+echo "CERT_PUB_KEY=$(sudo base64 -w 0 /opt/tactical-rmm/certs/certs/yourdomain.com.crt)" >> tactical-rmm/.env
+echo "CERT_PRIV_KEY=$(sudo base64 -w 0 /opt/tactical-rmm/certs/private/yourdomain.com.key)" >> tactical-rmm/.env
---
+++ Tactical-RMM-Saltbox/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Traefik Cert Dumper Install
+docker compose -f cert-dumper/docker-compose.yml up -d
+sleep 30
+
+# Copies Traefik rules ovwr for sub domain forwarding with nginx
+cp traefik/rmm.yml /opt/traefik/
+
+# user input to update domain names in tactical-rmm .env file and cert.sh
+echo "input needed for domain customization, please include full domain. example yourdomain.com"
+read -p "Enter your domain name: " domain
+sed -i "s/yourdomain.com/$domain/g" tactical-rmm/.env
+sed -i "s/yourdomain.com/$domain/g" cert-dumper/cert.sh
+echo "Updated values:"
+cat tactical-rmm/.env
+cat cert.sh
+
+# Updates .env fike with exported cert information
+sh cert-dumper/cert.sh
+
+# User input to update username in tactical-rmm .env file
+read -p "Enter your username: " username
+sed -i "s/username/$username/g" tactical-rmm/.env
+echo "Updated values:"
+cat tactical-rmm/.env
+
+# User input to uodate password in tactical-rmm .env file
+read -p "Enter your password: " password
+sed -i "s/password/$password/g" tactical-rmm/.env
+echo "Updated values:"
+cat tactical-rmm/.env
+
+# Tactical-rmm docker compose install
+docker compose -f tactical-rmm/docker-compose.yml up -d 
+sleep 40
+
+# Updates mesh_data config.json file domain entries
+dashdomain="dash.${domain}"
+sed -i "s/172.50.0.200/$dashdomain/g" /opt/tactical-rmm/mesh_data/config.json
+
+# Restarts mesh central containee
+docker restart trmm-meshcentral

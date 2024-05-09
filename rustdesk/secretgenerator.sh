de2503bea7e91d17c371f99d459d3eeec74ef71a
falco1717 <74680727+falco1717@users.noreply.github.com>
2024-05-09 08:48:22 -0700

    Update secretgenerator.sh

--- rustdesk/secretgenerator.sh
+++ rustdesk/secretgenerator.sh
@@ -5,6 +5,3 @@ RANDOM_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 
 # Write the random key to .env file
 echo "SECRET_KEY=$RANDOM_KEY" > .env
-
-# Run docker-compose
-docker-compose up -d

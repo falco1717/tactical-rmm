0f6fa733f57d357004fdfc9d342e11b288810542
falco1717 <74680727+falco1717@users.noreply.github.com>
2024-05-09 08:35:42 -0700

    Add files via upload

---
+++ rustdesk/secretgenerator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Generate a random key
+RANDOM_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
+
+# Write the random key to .env file
+echo "SECRET_KEY=$RANDOM_KEY" > .env
+
+# Run docker-compose
+docker-compose up -d
